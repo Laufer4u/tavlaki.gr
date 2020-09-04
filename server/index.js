883014a63c70bcdd190daf69d7407a7cfbca86fd
Ilias Apostolou <ilias.apostolou.zero@gmail.com>
2020-09-04 13:44:32 +0300

    +

--- server/index.js
+++ server/index.js
@@ -6,5 +6,5 @@ console.log('Server Started at: ', now_date);
 
 // Run a fuction every 1000 ms.
 setInterval(() => {
-    console.log('tick', now_ms / 1000); // Log the unix second timestamp.
+    //console.log('tick', Date.now() / 1000); // Log the unix second timestamp.
 }, 1000);
